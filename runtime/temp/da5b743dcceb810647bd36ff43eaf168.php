341f53ec9c50de1bcb004c8ca38cecc4dbb6021b
lzycode123 <63543517+lzycode123@users.noreply.github.com>
2020-04-17 14:38:06 +0800

    dd

---
+++ runtime/temp/da5b743dcceb810647bd36ff43eaf168.php
@@ -0,0 +1,14 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:75:"E:\phpstudy_pro\WWW\pblog\public/../application/index\view\index\index.html";i:1587015238;}*/ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>营销号</title>
+</head>
+<body>
+    <form action="index()" method="get">
+        <input type="text" autocomplete="off" name="name" />
+        <input type="submit" value="submit" />
+    </form>
+</body>
+</html>
